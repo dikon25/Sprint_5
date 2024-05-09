3acd469a811fe0f15cf33e07e45fbc179129fee5
Дмитрий Кондаков <dikon25@gmail.com>
2024-05-10 01:30:48 +0300

    bugs fixed

--- Lockator.py
+++ Lockator.py
@@ -8,48 +8,49 @@ class Register:
     # Поле ввода PASSWORD
     r_password_fild=(By.XPATH,".//input [@type='password']")
     #Кнопка ЗАРЕГИСТРИРОВАТЬСЯ на странице https://stellarburgers.nomoreparties.site/register
-    r_registration_button=(By.XPATH,'//*[@id="root"]/div/main/div/form/button[text()="Зарегистрироваться"]')
+    r_registration_button=(By.XPATH,'//button[text()="Зарегистрироваться"]')
     # Кнопка ВОЙТИ на страницеhttps://stellarburgers.nomoreparties.site/register
-    r_enter_button=(By.XPATH,'//*[@id="root"]/div/main/div/div/p/a')
-    r_error_password=(By.XPATH,".//*[@id='root']/div/main/div/form/fieldset[3]/div/p")
+    r_enter_button=(By.XPATH,'//a[@href="/login"]')
+    # сообщение НЕККОРЕТ+КТНЫЙ ПАРОЛЬ на странице https://stellarburgers.nomoreparties.site/register
+    r_error_password=(By.XPATH,'//p[text()="Некорректный пароль"]')
 class Button:
-    #Войти в аккаунт на главной странице
-    PLACE_ORDER=(By.XPATH,'.//*[@id="root"]/div/main/section[2]/div/button')
-    #Кнопка ВОЙТИ на странице логин
-    ENTER=(By.XPATH,'//*[@id="root"]/div/main/div/form/button[text()="Войти"]')
-    #Надпись ВХОД на странице логин
-    LOGIN_TEXT=(By.XPATH, '//*[@id="root"]/div/main/div/h2')
-    #Кнопка ЛИЧНЫЙ КАБИНЕТ в хедере
-    PERSONAL_ACCOUNT=(By.XPATH,'// *[ @ id = "root"] / div / header / nav / a / p')
     #Кнопка ОФОРМИТЬ ЗАКАЗ на главной странице после регистрации
-    ENTER_ACCOUNT=(By.XPATH,'//*[@id="root"]/div/main/section[2]/div/button')
+    PLACE_ORDER=(By.XPATH,'//button[text()="Оформить заказ"]')
+    #Кнопка ВОЙТИ на странице логин
+    ENTER=(By.XPATH,'//button[text()="Войти"]')
+    #Надпись ВХОД на странице логин
+    LOGIN_TEXT=(By.XPATH, '//h2[text()="Вход"]')
+    #Кнопка ЛИЧНЫЙ КАБИНЕТ в хедере
+    PERSONAL_ACCOUNT=(By.XPATH,'//p[text()="Личный Кабинет"]')
+    # Войти в аккаунт на главной странице
+    ENTER_ACCOUNT=(By.XPATH,'.//button[text()="Войти в аккаунт"]')
     #Кнопка ЗАРЕГИСТРИРОВАТЬСЯ на странице логин
-    ENTER_FROM_REGISTRAITE=(By.XPATH,'// *[ @ id = "root"] / div / main / div / div / p / a')
+    ENTER_FROM_REGISTRAITE=(By.XPATH,'// a [@href="/register"]')
     # Кнопка ВОССТАНОВИТЬ ПАРОЛЬ на странице логин
-    RECOVER_PASS=(By.XPATH,'//*[@id="root"]/div/main/div/div/p[2]/a')
+    RECOVER_PASS=(By.XPATH,'// a[@href="/forgot-password"]')
     #Кнопка ВОССТАНОВИТЬ на странице https://stellarburgers.nomoreparties.site/forgot-password
-    RECOVER_BUTTON=(By.XPATH,'// *[ @ id = "root"] / div / main / div / form / button')
+    RECOVER_BUTTON=(By.XPATH,'// button[text()="Восстановить"]')
     # Кнопка ВОЙТИ на странице https://stellarburgers.nomoreparties.site/forgot-password
-    ENTER_FROM_RECOVER=(By.XPATH,'//*[@id="root"]/div/main/div/div/p/a')
+    ENTER_FROM_RECOVER=(By.XPATH,'//a[@href="/login"]')
     #Кнопка ПРОФИЛЬ на странице https://stellarburgers.nomoreparties.site/account/profile
-    PROFIL=(By.XPATH,'//*[@id="root"]/div/main/div/nav/ul/li[1]/a')
+    PROFIL=(By.XPATH,'//a[@href="/account/profile"]')
     # Кнопка КОНСТРУКТОР в хедере
-    CONSTRUCTOR_BUTTON=(By.XPATH,'// *[ @ id = "root"] / div / header / nav / ul / li[1] / a / p')
+    CONSTRUCTOR_BUTTON=(By.XPATH,'// p[text()="Конструктор"]')
     #Надпись СОБЕРИТЕ БУРГЕР на главной странице
-    COLLECT_BREAD=(By.XPATH,'// *[ @ id = "root"] / div / main / section[1] / h1')
+    COLLECT_BREAD=(By.XPATH,'//h1[text()="Соберите бургер"]')
     #Кнопка ВЫХОД на странице https://stellarburgers.nomoreparties.site/account/profile
-    BUTTON_EXIT=(By.XPATH,'// *[ @ id = "root"] / div / main / div / nav / ul / li[3] / button')
+    BUTTON_EXIT=(By.XPATH,'// button[text()="Выход"]')
     #Кнопка СОУСЫ на главной странице
-    SAUCE=(By.XPATH,'// *[ @ id = "root"] / div / main / section[1] / div[1] / div[2] / span')
+    SAUCE=(By.XPATH,'// span[text()="Соусы"]')
     # Кнопка НАЧИНКИ на главной странице
-    FILLINGS=(By.XPATH,'// *[ @ id = "root"] / div / main / section[1] / div[1] / div[3] / span')
+    FILLINGS=(By.XPATH,'// span[text()="Начинки"]')
     # Кнопка БУЛКИ на главной странице
-    ROLLS=(By.XPATH,'// *[ @ id = "root"] / div / main / section[1] / div[1] / div[1] / span')
+    ROLLS=(By.XPATH,'//span[text()="Булки"]')
     # Надпись СОУСЫ на главной странице в зоне скролла
-    SAUCE_SCROLL_TEXT=(By.XPATH,'// *[ @ id = "root"] / div / main / section[1] / div[2] / h2[2]')
+    SAUCE_SCROLL_TEXT=(By.XPATH,'// h2[2][text()="Соусы"]')
     # Надпись НАЧИНКИ на главной странице в зоне скролла
-    FILLINGS_SCROLL_TEXT=(By.XPATH,'//*[@id="root"]/div/main/section[1]/div[2]/h2[3]')
+    FILLINGS_SCROLL_TEXT=(By.XPATH,'//h2[3][text()="Начинки"]')
     # Надпись БУЛКИ на главной странице в зоне скролла
-    ROLLS_SCROLL_TEXT=(By.XPATH,'//*[@id="root"]/div/main/section[1]/div[2]/h2[1]')
+    ROLLS_SCROLL_TEXT=(By.XPATH,'//h2[1][text()="Булки"]')
     #Кнопка РЕГИСТРАЦИЯ на станице https://stellarburgers.nomoreparties.site/login
-    REGESTRATION_BUTTON=(By.XPATH,'// *[ @ id = "root"] / div / main / div / div / p[1] / a')
+    REGESTRATION_BUTTON=(By.XPATH,'// a[@href="/register"]')
--- tests/GoToPersonalAccount.py
+++ tests/GoToPersonalAccount.py
@@ -5,15 +5,15 @@ from Lockator import *
 from data import Data_url
 from data_login import *
 
-
-def test_transfer_personal_account_open_page_profile(driver):
-    driver.get(Data_url.url_main_paige)
-    driver.find_element(*Button.PERSONAL_ACCOUNT).click()
-    name, email, password = Sing_up_data_rnd()
-    driver.find_element(*Register.r_email_fild).send_keys(email)
-    driver.find_element(*Register.r_password_fild).send_keys(password)
-    driver.find_element(*Button.ENTER).click()
-    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Button.PLACE_ORDER))
-    driver.find_element(*Button.PERSONAL_ACCOUNT).click()
-    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Button.PROFIL))
-    assert driver.current_url==Data_url.url_profile
+class TestGoToPersonalAccount:
+    def test_transfer_personal_account_open_page_profile(self,driver):
+        driver.get(Data_url.url_main_paige)
+        driver.find_element(*Button.PERSONAL_ACCOUNT).click()
+        name, email, password = Sing_up_data_rnd()
+        driver.find_element(*Register.r_email_fild).send_keys(email)
+        driver.find_element(*Register.r_password_fild).send_keys(password)
+        driver.find_element(*Button.ENTER).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Button.PLACE_ORDER))
+        driver.find_element(*Button.PERSONAL_ACCOUNT).click()
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Button.PROFIL))
+        assert driver.current_url==Data_url.url_profile
